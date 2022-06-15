b3bf8cc4c55e6c8b2463ec2e100b79fc5fc3cec6
Christopher McCulloh <cmcculloh@gmail.com>
2022-06-15 19:10:18 -0400

    Adds test-json-import package so we can test importing .json from an npm module

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,11 +1,24 @@
-import {
-	findwidth
-} from '../index';
+import data from "test-json-import/data.json" assert { type: "json" };
+import { findwidth, getData, showMeTheData } from "../index.js";
 
-describe('width', () => {
-	it('finds width', () => {
-		const width = findwidth('a');
-		console.log('width', width);
+describe("width", () => {
+	it("finds width", () => {
+		const width = findwidth("a");
+		console.log("width", width);
 		expect(width).toBe(1);
+	});
+});
+
+describe("json import", () => {
+	it("works", () => {
+		const dataStr = getData();
+		expect(dataStr).toBe(JSON.stringify(data));
+	});
+});
+
+describe("json imports within modules", () => {
+	it("work", () => {
+		const dataStr = showMeTheData();
+		expect(dataStr).toBe(`{"id":1}`);
 	});
 });
--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-// Note that this imports an npm package that also uses import statements. Therefore, you will have to add 'string-width' to jest.config.json in the transformIgnorePatterns array.
-import stringWidth from 'string-width';
+// Note that this imports an npm package that also uses import statements.
+// Therefore, you will have to add 'string-width' to jest.config.json in the transformIgnorePatterns array.
+import stringWidth from "string-width";
+import data from "./node_modules/test-json-import/data.json" assert { type: "json" };
+import { showMeTheData } from "test-json-import/index.js";
+
+const getData = () => {
+	console.log(JSON.stringify(data));
+	return JSON.stringify(data);
+};
 
 const findwidth = (a) => {
 	return stringWidth(a);
-}
+};
 
-export { findwidth };
+export { findwidth, getData, showMeTheData };
