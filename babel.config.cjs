f84830bf587079b8ea87f0fd62b24b32533e2c37
Christopher McCulloh <cmcculloh@gmail.com>
2022-06-15 19:09:43 -0400

    Adds babel plugin to babel config

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,7 +1,4 @@
 module.exports = {
-    "presets": [
-        [
-            "@babel/preset-env"
-        ]
-    ]
-}
+	presets: [["@babel/preset-env"]],
+	plugins: ["@babel/plugin-syntax-import-assertions"],
+};
